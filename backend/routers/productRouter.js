caaf239e3ff7ef6a58bf53f7d8e3b5232f8e48c7
Jihyunnk <kimregina2757@gmail.com>
2022-06-03 13:18:26 -0400

    created router for get all products

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -6,6 +6,14 @@ import Product from '../models/productModel.js';
 const productRouter = express.Router();
 
 productRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+productRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
     const products = await Product.insertMany(data.products);
