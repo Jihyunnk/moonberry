bafb164a2a5e57ba9b062fdfa544db166b923b07
Jihyunnk <kimregina2757@gmail.com>
2022-06-07 15:02:52 -0400

    styled Cart display

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -34,81 +34,85 @@ function Cart() {
       <div>
         <h1 className="cart-header">YOUR CART</h1>
       </div>
-      <div>
-        <div className="card card-body item-content">
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.product}>
-                <div className="row">
-                  <div>
-                    <img src={item.image} alt={item.name} className="small" />
-                  </div>
-                  <div className="min-25">
-                    <Link to={`/products/${item.product}`}>{item.name}</Link>
-                  </div>
-                  <div className="row item-details">
+      <div className="row">
+        <div className="col-2">
+          <div className="card card-body item-content">
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.product}>
+                  <div className="row">
                     <div>
-                      <select
-                        value={item.quantity}
-                        onChange={(e) =>
-                          dispatch(
-                            addToCart(item.product, Number(e.target.value))
-                          )
-                        }
-                      >
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                      </select>
+                      <img src={item.image} alt={item.name} className="small" />
                     </div>
-                    <div>${item.price.toFixed(2)}</div>
+                    <div className="min-25">
+                      <Link to={`/products/${item.product}`}>{item.name}</Link>
+                    </div>
+                    <div className="row item-details">
+                      <div>
+                        <select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            dispatch(
+                              addToCart(item.product, Number(e.target.value))
+                            )
+                          }
+                        >
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                      </div>
+                      <div>${item.price.toFixed(2)}</div>
+                    </div>
                   </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="col-1 order-summary-wrapper">
+          <div className="card card-body order-summary">
+            <ul>
+              <li>
+                <p>Order Summary</p>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Subtotal</div>
+                  <div>${itemsPrice.toFixed(2)}</div>
                 </div>
               </li>
-            ))}
-          </ul>
-        </div>
-        <div className="card card-body">
-          <ul>
-            <li>
-              <p>Order Summary</p>
-            </li>
-            <li>
-              <div className="row">
-                <div>Subtotal</div>
-                <div>${itemsPrice.toFixed(2)}</div>
-              </div>
-            </li>
-            <li>
-              <div className="row">
-                <div>Estimated Shipping</div>
-                <div>${shippingPrice.toFixed(2)}</div>
-              </div>
-            </li>
-            <li>
-              <div className="row">
-                <div>Estimated Tax</div>
-                <div>${taxPrice.toFixed(2)}</div>
-              </div>
-            </li>
-            <li>
-              <div className="row">
-                <div>Total</div>
-                <div>${totalPrice.toFixed(2)}</div>
-              </div>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="primary"
-                disabled={cartItems.length === 0}
-              >
-                Proceed to checkout
-              </button>
-            </li>
-          </ul>
+              <li>
+                <div className="row">
+                  <div>Estimated Shipping</div>
+                  <div>${shippingPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Estimated Tax</div>
+                  <div>${taxPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div className="total">Total</div>
+                  <div>${totalPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="primary"
+                  disabled={cartItems.length === 0}
+                >
+                  Proceed to checkout
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
