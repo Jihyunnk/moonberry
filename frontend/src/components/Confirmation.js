6754aed3e8c7e984d2fccdcaea98fb328acbd53f
Jihyunnk <kimregina2757@gmail.com>
2022-06-09 16:46:28 -0400

    displayed order details in confirmation

--- frontend/src/components/Confirmation.js
+++ frontend/src/components/Confirmation.js
@@ -1,66 +1,91 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getOrder } from '../store/orderDetails';
 function Confirmation() {
-  const cart = useSelector((state) => state.cart);
-  const { shippingInfo, cartItems } = cart;
+  // const cart = useSelector((state) => state.cart);
+  // const { shippingInfo, cartItems } = cart;
+  const params = useParams();
+  const { id: orderId } = params;
+
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order } = orderDetails;
+  console.log('order: ', order);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrder(orderId));
+  }, [dispatch, orderId]);
 
   return (
     <div>
-      <div className="confirmation-wrapper">
-        <h1 className="confirmation-message">
-          Thank you for shopping with us!
-        </h1>
-        <div className="card card-body confirmation-card">
-          <h2>Order</h2>
-          <p>Your order is confirmed.</p>
-          <p>You'll receive an email when your order is ready.</p>
-        </div>
-        <div className="card card-body confirmation-card">
-          <h2>Customer Information</h2>
-          <div className="customer-info">
-            <div>
-              <p>Customer Name</p>
-              <p>
-                {shippingInfo.firstName} {shippingInfo.lastName}
-              </p>
-              <br />
-              <p>Shipping Address</p>
-              <p>
-                {shippingInfo.address}, {shippingInfo.city},{' '}
-                {shippingInfo.postalCode}, {shippingInfo.country}
-              </p>
+      {order._id ? (
+        <div>
+          <div className="confirmation-wrapper">
+            <h1 className="confirmation-message">
+              Thank you for shopping with us!
+            </h1>
+            <div className="card card-body confirmation-card">
+              <h2>Order</h2>
+              <p>Your order is confirmed.</p>
+              <p>You'll receive an email when your order is ready.</p>
             </div>
-            <div>
-              <p>Payment Method</p>
-              <p>Credit Card</p>
-              <br />
-              <p>Billing Address</p>
-              <p>
-                {shippingInfo.address}, {shippingInfo.city},{' '}
-                {shippingInfo.postalCode}, {shippingInfo.country}
-              </p>
+            <div className="card card-body confirmation-card">
+              <h2>Customer Information</h2>
+              <div className="customer-info">
+                <div>
+                  <p>Customer Name</p>
+                  <p>
+                    {order.shippingInfo.firstName} {order.shippingInfo.lastName}
+                  </p>
+                  <br />
+                  <p>Shipping Address</p>
+                  <p>
+                    {order.shippingInfo.address}, {order.shippingInfo.city},{' '}
+                    {order.shippingInfo.postalCode},{' '}
+                    {order.shippingInfo.country}
+                  </p>
+                </div>
+                <div>
+                  <p>Payment Method</p>
+                  <p>Credit Card</p>
+                  <br />
+                  <p>Billing Address</p>
+                  <p>
+                    {order.shippingInfo.address}, {order.shippingInfo.city},{' '}
+                    {order.shippingInfo.postalCode},{' '}
+                    {order.shippingInfo.country}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="card card-body border confirmation-card">
+              <h2>Order Items</h2>
+              <ul>
+                {order.orderItems.map((item) => (
+                  <li key={item.product}>
+                    <div>
+                      <div className="row">
+                        <div className="row">
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="small"
+                          />
+                          <p>{item.name}</p>
+                        </div>
+                        <p>${item.price.toFixed(2)}</p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-        <div className="card card-body border confirmation-card">
-          <h2>Order Items</h2>
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.product}>
-                <div>
-                  <div className="row">
-                    <div className="row">
-                      <img src={item.image} alt={item.name} className="small" />
-                      <p>{item.name}</p>
-                    </div>
-                    <p>${item.price.toFixed(2)}</p>
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      ) : (
+        <div>loading</div>
+      )}
     </div>
   );
 }
