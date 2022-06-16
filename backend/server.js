d961804c22449d8ffddc47490894bbc31ed3db8b
Jihyunnk <kimregina2757@gmail.com>
2022-06-16 11:41:39 -0400

    restructured files for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -12,14 +13,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/moonberry');
+// mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/moonberry');
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/confirmation', orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('server is ready');
+const __dirname = path.resolve();
+app.use(express.static(path.resolve(__dirname, './frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './frontend/build/index.html'));
 });
 
 app.use((err, req, res, next) => {
